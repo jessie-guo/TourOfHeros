927a4439451bee17713b6d64ce505b75756e1aee
Jessie Guo <jessie.guo@nelnet.net>
2020-05-21 12:18:44 -0500

    Created hero object. App and Hero components are bound to the App module. Hero name text box and two way binding added.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
-@Component({
-  selector: 'app-heroes',
+@Component({ //decorator function that specifies the Angular metadata for the component
+  selector: 'app-heroes', //'app-heroes', matches the name of the HTML element that identifies this component within a parent component's template
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
   constructor() { }
 
   ngOnInit(): void {
